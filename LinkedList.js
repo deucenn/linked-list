7907d817ab48089ef8fb4e7fd4548e77181d35f8
deucenn <panix.black@web.de>
2024-11-08 16:15:38 +0100

    Initial commit

---
+++ LinkedList.js
@@ -0,0 +1,37 @@
+class Node {
+  constructor() {
+    this.value = null;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {}
+
+  prepend(value) {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+
+  insertAt(index, value) {}
+
+  removeAt(index) {}
+}
